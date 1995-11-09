f3bd05789eeb247fc2fc2b39a2600da878bbb788
stephene <empty>
1995-11-09 20:59:27 +0000

    Initial revision

---
+++ dispnet.h
@@ -0,0 +1,32 @@
+/****************************************************************************
+***
+*** Time-stamp: <>
+***
+*** dispnet.h
+*** 
+*** Stephen Eglen
+*** COGS, University of Sussex.
+***
+*** Created 09 Nov 95
+***
+*** $Revision$
+*** $Date$
+****************************************************************************/
+
+
+#ifndef _DISPNET_H
+#define _DISPNET_H
+
+/* Header file for the disparity network */
+
+/* By default, we will be using doubles for weights and activations */
+#define Real double
+
+
+#endif
+
+
+/*************************** Version Log ****************************/
+/* $Log$
+ *
+ */
