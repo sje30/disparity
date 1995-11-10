14c2345422c511780b2e04fdf61ab3f913c0ee6c
stephene <empty>
1995-11-10 21:59:00 +0000

    Initial revision

---
+++ dispvars.h
@@ -0,0 +1,35 @@
+/****************************************************************************
+***
+*** Time-stamp: <10 Nov 95 21:58:13 stephene>
+***
+*** dispvars.h
+*** 
+*** Stephen Eglen
+*** COGS, University of Sussex.
+***
+*** Created 10 Nov 95
+***
+*** $Revision$
+*** $Date$
+****************************************************************************/
+
+
+#ifndef _DISPVARS_H
+#define _DISPVARS_H
+
+
+/* Global Disparity Variables used by the lex file. */
+char netFile[100];		/* Name of the file that stores the network
+				 * structure information.
+				 * Default: default.net
+				 */
+char inputFile[100];		/* Name of the file where inputs can
+				 * be read in from
+				 */
+#endif
+
+
+/*************************** Version Log ****************************/
+/* $Log$
+ *
+ */
