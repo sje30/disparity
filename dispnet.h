21e2fb839fcf21d99e279c6ba9f06f8c96391701
stephene <empty>
1995-11-12 23:06:27 +0000

    Daily change

--- dispnet.h
+++ dispnet.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 ***
-*** Time-stamp: <>
+*** Time-stamp: <11 Nov 95 18:15:04 stephene>
 ***
 *** dispnet.h
 *** 
@@ -9,8 +9,8 @@
 ***
 *** Created 09 Nov 95
 ***
-*** $Revision: 1.1 $
-*** $Date: 1995/11/09 20:59:27 $
+*** $Revision: 1.2 $
+*** $Date: 1995/11/10 15:29:10 $
 ****************************************************************************/
 
 
@@ -24,33 +24,50 @@ typedef double Real;
 
 
 /* Do we need bias for a layer? */
-typedef  enum  { nobias, bias} BiasType;
+typedef  enum  { Nobias, Bias} BiasType;
 
 /* Possible types of activation function */
-typedef  enum {linearfn, tanhfn} ActFn;
+typedef  enum {Linearfn, Tanhfn} ActFn;
 
 typedef struct {
   int  numInputs;
   int *inputs;
   Real *wtsStart;
-} CellInfo;
+} cellInfo_t;
 
+/* cellInfo_t:	Tells a cell where its input is coming from, and how
+ *		many inputs there are.  inputs[i] stores the location
+ *		of the input cell in the activations array that is
+ *		part of the input to this cell.
+ */
 
-typedef struct {
-  int      ncells;
-  int      nrows;
-  int      ncols;
-  CellInfo *cellInfo;
-  ActFn    actfn;
-  BiasType bias;
-} LayerInfo;
-
+/* layerInfo_t	Information for a layer. */
 
 typedef struct {
-  Real	*data;
-  int	nextfreeweight;
-  int	maxindex;
-} WeightInfo;
+  int       	ncells;		/* this equals nrows * ncols */
+  int       	nrows;		/* same as the ht */
+  int       	ncols;		/* same as the wid */
+  cellInfo_t 	*cellInfo;	/* cellInfo[i] stores the input
+				   information for cell i in this
+				   layer */
+  ActFn     	actfn;		/* Which activation function cells in this
+				   layer use. */
+  BiasType	 bias;		/* Does this layer provide bias for
+				   the next layer?*/ 
+} layerInfo_t;
+
+
+
+/* weightInfo_t:	Information about the weight structure */
+typedef struct {
+  Real	*data;		/* Actual weight vector */
+  int	nextFreeWeight;	/* Index to next free weight element, as
+			 * weights are being allocated by the function
+			 *  nextFreeWeight() */			   
+  int	maxIndex;	/* Maximum index into the weight vector */
+  int	numWts;		/* Number of weights allocated.
+			 * ie, last weight stored in data[numWts-1] */
+} weightInfo_t;
 
 
 
@@ -59,7 +76,7 @@ typedef struct {
   int  *startLayer;
   int  *biasIndex;
   int  size;
-} ActivationInfo;
+} activationInfo_t;
 
 /* ActivationInfo:	Stores the activation level of the cells 
  * 
@@ -73,38 +90,38 @@ typedef struct {
  *			equal 99999 to try and cause an error.
  *
  * size			size of the activation array, ie. actn[0 to size-1].
- *
- */
+ * */
     
  
 typedef struct {
-  int tlx;
-  int tly;
-  int brx;
-  int bry;
+  int tlx;	int tly;	/* Top Left */
+  int brx;	int bry;	/* Bottom Right */
 } Rect;
 
-typedef struct {
-  int nLayers;
-} NetInfo;
 
 
-/* NetInfo:	General information about the network.
+/* netInfo_t:	General information about the network.
  *
  * nLayers -  number of layers in the network. These will be labelled
  *            from 0 to the Nlayers-1. Layer 0 is the input layer.  */
 
+typedef struct {
+  int nLayers;
+} netInfo_t;
 
+
+
+/*** Function definitions ***/
+void calcAllActivations();
+void calcActivation(int layer);
+void clearUpMemory();
 
 /************************* Global Variables *************************/
-WeightInfo weightInfo;
-NetInfo	netInfo;
-ActivationInfo actInfo;
-LayerInfo *layerInfo;
+weightInfo_t	weightInfo;
+netInfo_t	netInfo;
+activationInfo_t actInfo;
+layerInfo_t	*layerInfo;
 /************************* Global Variables *************************/
-
-#define AND &&
-#define OR ||
 
 
 
@@ -114,6 +131,12 @@ LayerInfo *layerInfo;
 
 /*************************** Version Log ****************************/
 /* $Log: dispnet.h,v $
+ * Revision 1.2  1995/11/10  15:29:10  stephene
+ * Daily update - next major step is to present net with input and
+ * calculate activations.
+ *
+ * Will also need to initialise weights.
+ *
  * Revision 1.1  1995/11/09  20:59:27  stephene
  * Initial revision
  *
