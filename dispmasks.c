ca0eb2d4c02c5074d647a4c2b99b55ac5c5bc2c7
stephene <empty>
1995-11-20 14:43:28 +0000

    Initial revision

---
+++ dispmasks.c
@@ -0,0 +1,42 @@
+/****************************************************************************
+***
+***
+*** dispmasks.c
+*** 
+*** Stephen Eglen
+*** COGS, University of Sussex.
+***
+*** Created 20 Nov 95
+***
+*** $Revision$
+*** $Date$
+****************************************************************************/
+
+
+#ifndef lint
+static char *rcsid = "$Header$";
+#endif
+
+
+/* Functions relating to the masks used for convolutions. */
+
+
+/* -  Include Files - */
+
+/* - Defines - */
+
+/* - Function Declarations - */
+
+
+/* - Global Variables - */ 
+
+
+
+/* - Start of Code  - */
+
+
+/*************************** Version Log ****************************/
+/*
+ * $Log$
+ */
+
