74dc23b131f12c2b737d2079b97e66ae1e19b930
stephene <empty>
1995-11-22 00:54:50 +0000

    Initial revision

---
+++ testnet.h
@@ -0,0 +1,25 @@
+/****************************************************************************
+***
+***
+*** testnet.h
+*** 
+*** Stephen Eglen
+*** COGS, University of Sussex.
+***
+*** Created 22 Nov 95
+***
+*** $Revision$
+*** $Date$
+****************************************************************************/
+
+
+#ifndef _TESTNET_H
+#define _TESTNET_H
+void copyVec(Real *wdest, Real *wsrc, int numWeights);
+#endif
+
+
+/*************************** Version Log ****************************/
+/* $Log$
+ *
+ */
