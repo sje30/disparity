01e31a055f1fd968f1bfca402b8c016dadbc7b2d
Stephen J. Eglen <sje30@cam.ac.uk>
2021-03-05 10:20:35 +0000

    make global variables "extern" declarations.

--- dispvars.h
+++ dispvars.h
@@ -19,108 +19,108 @@
 
 
 /* Global Disparity Variables used by the lex file. */
-char netFile[100];		/* Name of the file that stores the network
+extern char netFile[100];		/* Name of the file that stores the network
 				 * structure information.
 				 * Default: default.net
 				 */
-char inputFile[100];		/* Name of the file where inputs can
+extern char inputFile[100];		/* Name of the file where inputs can
 				 * be read in from.
 				 */
 
-double ulambda;			/* Lambda for short range mean U */
-double vlambda;			/* Lambda for long range mean V */
+extern double ulambda;			/* Lambda for short range mean U */
+extern double vlambda;			/* Lambda for long range mean V */
 				/* The width of the means for U and V
                                  * can either be set using lambda, or
                                  * by providing the half lives
                                  * directly. See useHalf. uhalf and vhalf. */
 
 
-int outputWid; 			/* Width of the output cells. See also
+extern int outputWid; 			/* Width of the output cells. See also
 				 * outputHt. */
 
-int outputHt;			/* Height of the output cells. For a
+extern int outputHt;			/* Height of the output cells. For a
 				 * 1d arrangement of output cells,
 				 * this should be set to 1.  If this
 				 * value is not one, it is assumed
 				 * that the network is 2d.  */
 
-int totalInputWid;		/* Total width of Jim's input files.
+extern int totalInputWid;		/* Total width of Jim's input files.
 				 * Default to 7000. */
-int totalInputHt;		/* Total height of Jim's input files.
+extern int totalInputHt;		/* Total height of Jim's input files.
 				 * Default to 5. */
-char  image1File[100]; 		/* File for left image */
-char  image2File[100]; 		/* File for right image */
-char  shiftsFile[100];		/* File for shifts data */
-int numInputVectors;		/* number of input vectors to make
+extern char  image1File[100]; 		/* File for left image */
+extern char  image2File[100]; 		/* File for right image */
+extern char  shiftsFile[100];		/* File for shifts data */
+extern int numInputVectors;		/* number of input vectors to make
 				   from the total input files. */
 
-int inputSkipX;			/* How many columns to skip between
+extern int inputSkipX;			/* How many columns to skip between
 				 * successive input vectors. Default 2.
 				 */
-int inputSkipY; 		/* How many rows to skip between input
+extern int inputSkipY; 		/* How many rows to skip between input
 				 * vectors from a 2d image. Default 0.
 				 */
 
-int inputHt; 			/* Height of each input vector from
+extern int inputHt; 			/* Height of each input vector from
 				   one image */
-int inputWid; 			/* Width of each input vector from one im. */
+extern int inputWid; 			/* Width of each input vector from one im. */
 
-int cgmax;			/* If non zero, then maximise merit
+extern int cgmax;			/* If non zero, then maximise merit
 				 * function in conjugate gradient
 				 * method. Default is 1.
 				 */
-int checker;			/* Do we use conjugate gradient or
+extern int checker;			/* Do we use conjugate gradient or
 				 * just check the derivatives?  Non
 				 * zero means that we check.
 				 * Default: 1.
 				 */
 
-int maxiterations;		/* Maximum number of iterations for CG
+extern int maxiterations;		/* Maximum number of iterations for CG
 				 * to perform.  Default is 100. */
 
-int useHalf;		        /* If nonzero, use uhalf and vhalf to
+extern int useHalf;		        /* If nonzero, use uhalf and vhalf to
 				 * specify the half lives for U and
 				 * V.  Otherwise, use ulambda and
 				 * vlambda.  Default is 0. */
-int uhalf, vhalf; 		/* Half lives of U and V. If useHalf
+extern int uhalf, vhalf; 		/* Half lives of U and V. If useHalf
 				 * is non zero, then these values are
 				 * used to specify the size of U and
 				 * V. */
 
-char  initWts[100];		/* Name of file storing the initial
+extern char  initWts[100];		/* Name of file storing the initial
 				 * weights of the network if any.  If
 				 * value = "none" (default value),
 				 * then rnd initial weights are
 				 * created. */
 
-char  results[100];		/* Where to put the results of
+extern char  results[100];		/* Where to put the results of
 				 * checking the nets
 				 * performanace. Default "results". */
 
 
-int doLearning;			/* If non zero (default), then do
+extern int doLearning;			/* If non zero (default), then do
 				 * learning. Otherwise, just read in
 				 * initial weights and run input
 				 * images through the network, and
 				 * calculate the correlation. */
 
-int oneImage;			/* Do we want to read in one image or two?
+extern int oneImage;			/* Do we want to read in one image or two?
 				 * Default 0 - means we have more than one
 				 * image file. */
-int compcorrn;			/* Do we want to compute correlation?
+extern int compcorrn;			/* Do we want to compute correlation?
 				 * If non zero, correlation is computed.
 				 * Default: 1*/
-int usegnuplot;			/* Should we use gnuplot to display
+extern int usegnuplot;			/* Should we use gnuplot to display
 				 * network activity? If non zero, then
 				 * use gnuplot. Default: 0.  */
-int seed;			/* If non-zero, use this value as the seed.
+extern int seed;			/* If non-zero, use this value as the seed.
 				 * Otherwise, clock used to set seed.
 				 * Default 0.
 				 */
-int normInput;			/* Non-zero if we want to normalize each
+extern int normInput;			/* Non-zero if we want to normalize each
 				 * image-patch on way in. Default 0.
 				 */
-int noshifting;			/* Non-zero if we don't need to shift the
+extern int noshifting;			/* Non-zero if we don't need to shift the
 				 * desired output values when computing
 				 * the correlation between actual and
 				 * desired output.
